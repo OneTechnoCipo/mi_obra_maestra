49414589335a612f88d89bd3f5782b9b2f5e279b
OneTechnoCipo <marianocollazo2003@gmai.com>
2022-04-11 10:48:15 -0300

    Primeros cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-const routes: Routes = [];
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+
+const routes: Routes = [/* inicio y equipo que van de componentes */
+{path:'inicio', component: InicioComponent},
+{path:'',component:InicioComponent},
+{path:'equipo',component:EquipoComponent},
+{path:'contacto',component:ContactoComponent},
+{path:'servicios',component:ServiciosComponent},
+{path:'proyectos',component:ProyectosComponent},
+{path:'**',component:InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { EquipoComponent } from './pages/equipo/equipo.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    BodyComponent,
+    InicioComponent,
+    EquipoComponent,
+    ContactoComponent,
+    ServiciosComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
